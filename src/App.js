bffc3a064d34f46815b3df1e8e1ac51a4499a2d9
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-31 22:02:07 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Fragment} from 'react'
+import Auth from './components/Auth';
+import Header from './components/Header'
+import UserProfile from './components/UserProfile';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <Header/>
+      <Auth/>
+      <UserProfile/>
+    </Fragment>
+  )
 }
 
 export default App;
