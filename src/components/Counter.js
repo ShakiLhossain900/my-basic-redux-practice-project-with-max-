b83c1cd16fd895fef1b6cb17572484100d415118
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-31 23:37:15 +0600

    adding

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,10 +3,12 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch =  useDispatch();
+
+  const counter = useSelector((state)=>state.counter.counter);
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}></div>
+      <div className={classes.value}>{counter}</div>
       <div >
         <button>Increment</button>
         <button>Increment by 5</button>
