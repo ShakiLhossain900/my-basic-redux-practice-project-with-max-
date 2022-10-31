3687e050e0a66310b095b096f9819eb9613ee92a
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-10-31 22:27:09 +0600

    add

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-    return (
-        <main>
-            <h1>Redux Counter</h1>
-            <div>
-                <button>Increment</button>
-                <button>Increment by 5</button>
-                <button>Decrement</button>
-            </div>
-            <button>Toggole Counter</button>
-        </main>
-    );
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}></div>
+      <div >
+        <button>Increment</button>
+        <button>Increment by 5</button>
+        <button>Decrement</button>
+      </div>
+      <button>Toggole Counter</button>
+    </main>
+  );
 };
 
 export default Counter;
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
-
+import {createSlice} from '@reduxjs/toolkit';
 
 //creating the store 
 
 //redux toolkit counter 
-const 
+const initailConterState={counter:0, showCounter:true};
+
+const counterSlice =({
+    name: 'counter',
+    initialState: initailConterState, 
+
+    reducers:{
+        increment(state){
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        increase(state, action){
+          state.counter = state.counter + action.payload;
+        },
+        toggleCounter(state){
+            state.showCounter= !state.showCounter
+        }
+        
+
+    }
+})
