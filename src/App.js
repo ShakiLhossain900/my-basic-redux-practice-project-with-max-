3da6a5daad423e5baf14898b28b3c78d4353a34d
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-01 00:43:43 +0600

    adding

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import {Fragment} from 'react'
+import { useSelector } from 'react-redux';
 import Auth from './components/Auth';
 import Counter from './components/Counter';
 import Header from './components/Header'
 import UserProfile from './components/UserProfile';
 function App() {
+const isAuth =  useSelector(state=>state.auth.isAuthenticated);
   return (
     <Fragment>
       <Header/>
-      <Auth/>
-      <UserProfile/>
+     {!isAuth && <Auth/>}
+      {isAuth && <UserProfile/>}
       <Counter/>
     </Fragment>
   )
